16a180e39f64709b6bd37ec6d7797bdba28cbe5f
Mario Geiger <geiger.mario@gmail.com>
2022-06-09 06:54:43 -0400

    remove problematic code

--- models/jax/main.py
+++ models/jax/main.py
@@ -3,7 +3,6 @@ import pickle
 import shutil
 import time
 from functools import partial
-from textwrap import dedent
 
 import haiku as hk
 import numpy as np
@@ -114,9 +113,6 @@ def main():
         w = optax.apply_updates(w, updates)
         return w, opt_state, loss, pred
 
-    last_code = None
-    var = locals()
-
     # Init
     print("Init statistics...", flush=True)
     time0 = time.perf_counter()
@@ -140,109 +136,84 @@ def main():
     t4 = time.perf_counter()
     # Init
 
-    for i in range(144_001):
-        var["i"] = i
+    for i in range(99_999_999):
+        # Loop
+        t0 = time.perf_counter()
+        t_extra = t0 - t4
 
-        ok = True
-        if ok:
-            for trial in range(10):
-                try:
-                    with open(f"{wandb.run.dir}/main.py", "r") as f:
-                        code = dedent(f.read().split("# Loop")[2])
-                    if code != last_code:
-                        with open(f"{wandb.run.dir}/main.py", "r") as f:
-                            init_code = dedent(f.read().split("# Init")[1])
-                        exec(init_code, globals(), var)
-                        last_code = code
-                    exec(code, globals(), var)
-                    ok = True
+        if i == 120:
+            jax.profiler.start_trace(wandb.run.dir)
+
+        img, lab, zooms = train_set[i % len(train_set)]
+
+        # regroup zooms and sizes by rounding and taking subsets of the volume
+        zooms = jax.tree_map(lambda x: round(433 * x) / 433, zooms)
+        if np.random.rand() < 0.5:
+            while True:
+                x, y = random_sample(img, lab, sample_size)
+                if np.any(un(y) == 1):
+                    img, lab = x, y
                     break
-                except Exception as e:
-                    ok = False
-                    print(f"Trial {trial} failed", flush=True)
-                    print(e)
-                    print("\nRetrying again in 1min...", flush=True)
-                    time.sleep(60)
-            if not ok:
-                raise Exception("Failed to run loop in 10 trials, aborting")
         else:
-            # Loop
-            t0 = time.perf_counter()
-            t_extra = t0 - t4
+            img, lab = random_sample(img, lab, sample_size)
 
-            if i == 120:
-                jax.profiler.start_trace(wandb.run.dir)
+        t1 = time.perf_counter()
 
-            img, lab, zooms = train_set[i % len(train_set)]
+        w, opt_state, train_loss, train_pred = update(w, opt_state, img, lab, zooms, args.lr)
+        train_loss.block_until_ready()
 
-            # regroup zooms and sizes by rounding and taking subsets of the volume
-            zooms = jax.tree_map(lambda x: round(433 * x) / 433, zooms)
-            if np.random.rand() < 0.5:
-                while True:
-                    x, y = random_sample(img, lab, sample_size)
-                    if np.any(un(y) == 1):
-                        img, lab = x, y
-                        break
-            else:
-                img, lab = random_sample(img, lab, sample_size)
+        t2 = time.perf_counter()
+        if i % 3 == 0:
+            img, lab, zooms = test_set[(i // 3) % 10]
+            test_pred = apply_model(w, img, zooms)
+            confusion_matrices.append(confusion_matrix(un(lab).flatten() > 0.0, un(test_pred).flatten() > 0.0))
 
-            t1 = time.perf_counter()
+        t3 = time.perf_counter()
+        epoch_avg_confusion = np.mean(confusion_matrices[-len(test_set) :], axis=0)
+        epoch_avg_confusion = epoch_avg_confusion / np.sum(epoch_avg_confusion)
 
-            w, opt_state, train_loss, train_pred = update(w, opt_state, img, lab, zooms, args.lr)
-            train_loss.block_until_ready()
+        min_median_max = np.min(train_pred), np.median(train_pred), np.max(train_pred)
+        t4 = time.perf_counter()
 
-            t2 = time.perf_counter()
-            if i % 3 == 0:
-                img, lab, zooms = test_set[(i // 3) % 10]
-                test_pred = apply_model(w, img, zooms)
-                confusion_matrices.append(confusion_matrix(un(lab).flatten() > 0.0, un(test_pred).flatten() > 0.0))
+        print(
+            (
+                f"{wandb.run.dir.split('/')[-2]} "
+                f"[{i + 1:04d}:{format_time(time.perf_counter() - time0)}] "
+                f"train[ loss={train_loss:.3f} "
+                f"tn={epoch_avg_confusion[0, 0]:.2f} tp={epoch_avg_confusion[1, 1]:.2f} "
+                f"fn={epoch_avg_confusion[1, 0]:.2f} fp={epoch_avg_confusion[0, 1]:.2f} "
+                f"min-median-max={min_median_max[0]:.2f} {min_median_max[1]:.2f} {min_median_max[2]:.2f} ] "
+                f"time="
+                f"S{format_time(t1 - t0)}"
+                f"U{format_time(t2 - t1)}+"
+                f"E{format_time(t3 - t2)}+C{format_time(t4 - t3)}+"
+                f"EX{format_time(t_extra)} "
+            ),
+            flush=True,
+        )
 
-            t3 = time.perf_counter()
-            epoch_avg_confusion = np.mean(confusion_matrices[-len(test_set) :], axis=0)
-            epoch_avg_confusion = epoch_avg_confusion / np.sum(epoch_avg_confusion)
+        state = {
+            "iteration": i,
+            "_runtime": time.perf_counter() - time0,
+            "train_loss": train_loss,
+            "true_negatives": epoch_avg_confusion[0, 0],
+            "true_positives": epoch_avg_confusion[1, 1],
+            "false_negatives": epoch_avg_confusion[1, 0],
+            "false_positives": epoch_avg_confusion[0, 1],
+            "min_pred": min_median_max[0],
+            "median_pred": min_median_max[1],
+            "max_pred": min_median_max[2],
+        }
 
-            min_median_max = np.min(train_pred), np.median(train_pred), np.max(train_pred)
-            t4 = time.perf_counter()
+        if i % 500 == 0:
+            with open(f"{wandb.run.dir}/w.pkl", "wb") as f:
+                pickle.dump(w, f)
 
-            print(
-                (
-                    f"{wandb.run.dir.split('/')[-2]} "
-                    f"[{i + 1:04d}:{format_time(time.perf_counter() - time0)}] "
-                    f"train[ loss={train_loss:.3f} "
-                    f"tn={epoch_avg_confusion[0, 0]:.2f} tp={epoch_avg_confusion[1, 1]:.2f} "
-                    f"fn={epoch_avg_confusion[1, 0]:.2f} fp={epoch_avg_confusion[0, 1]:.2f} "
-                    f"min-median-max={min_median_max[0]:.2f} {min_median_max[1]:.2f} {min_median_max[2]:.2f} ] "
-                    f"time="
-                    f"S{format_time(t1 - t0)}"
-                    f"U{format_time(t2 - t1)}+"
-                    f"E{format_time(t3 - t2)}+C{format_time(t4 - t3)}+"
-                    f"EX{format_time(t_extra)} "
-                ),
-                flush=True,
-            )
+        if i == 120:
+            jax.profiler.stop_trace()
 
-            state = {
-                "iteration": i,
-                "_runtime": time.perf_counter() - time0,
-                "train_loss": train_loss,
-                "true_negatives": epoch_avg_confusion[0, 0],
-                "true_positives": epoch_avg_confusion[1, 1],
-                "false_negatives": epoch_avg_confusion[1, 0],
-                "false_positives": epoch_avg_confusion[0, 1],
-                "min_pred": min_median_max[0],
-                "median_pred": min_median_max[1],
-                "max_pred": min_median_max[2],
-            }
-
-            if i % 500 == 0:
-                with open(f"{wandb.run.dir}/w.pkl", "wb") as f:
-                    pickle.dump(w, f)
-
-            if i == 120:
-                jax.profiler.stop_trace()
-
-            wandb.log(state)
-            # Loop
+        wandb.log(state)
+        # Loop
 
 
 if __name__ == "__main__":
